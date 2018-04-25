13cb1acb59ad1326bce4b0bb4c47f43418477853
Andrea Lara <anlara@csumb.edu>
2018-04-25 23:32:49 +0000

    changed file name to run on heroku

--- index.php
+++ index.php
@@ -1,43 +1,52 @@
-<!DOCTYPE html>
 <html>
-    <head>
-        <title>Hangman</title>
-        <link href="css/styles.css" rel="stylesheet" type="text/css" />
-        <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-        
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-    </head>
-    <body>
-        <div class='container text-center'>
-            <header>
-                <h1>Hangman</h1>
-                <h4>JavaScript Edition</h4>
-            </header>
-            
-            <div id="word"></div>
-            
-            <div id="letters"></div>
-            
-            <div id="won">
-                <h2>You Won!</h2>
-                <button class="replayBtn btn btn-success">Play Again</button>
-                
-            </div>
-            
-            <div id="lost">
-                <h2>You Lost!</h2>
-                <button class="replayBtn btn btn-warning">Play Again</button>
-            </div>
-            
-            <div id="man">
-                <img src="img/stick_0.png" id="hangImg">
-            </div>
-        </div>
-    <script src='js/hangman.js'></script>
-    </body>
-    
+	<head>
+		<title>Rock Paper Scissors</title>
+		<link rel="stylesheet" type="text/css" href="rps.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script language="javascript" type="text/javascript" src="rps.js"></script>
+	</head>
+	<body onload="init()">
+		<div id="gameWindow">
+			<div id="btnGo">GO</div>
+
+			<div id="playerPanel">
+				<h2>Player</h2>
+				<img id="imgPlayer" src="images/question.png">
+				<br><br>
+				<div id="btnRock" class="iconButton" onclick="selectRock()">
+					<img src="images/rock.png">
+					<br>Rock
+				</div>
+				<div id="btnPaper" class="iconButton" onclick="selectPaper()">
+					<img src="images/paper.png">
+					<br>Paper
+				</div>
+				<div id="btnScissors" class="iconButton" onclick="selectScissors()">
+					<img src="images/scissors.png">
+					<br>Scissors
+				</div>
+			</div>
+
+			<div id="computerPanel">
+				<h2>Computer</h2>
+				<img id="imgComputer" src="images/question.png">
+				<br><br>
+				<div id="lblRock" class="iconLabel">
+					<img src="images/rock.png">
+					<br>Rock
+				</div>
+				<div id="lblPaper" class="iconLabel">
+					<img src="images/paper.png">
+					<br>Paper
+				</div>
+				<div id="lblScissors" class="iconLabel">
+					<img src="images/scissors.png">
+					<br>Scissors
+				</div>
+			</div>
+
+			
+		</div>
+	</body>
 </html>
+
